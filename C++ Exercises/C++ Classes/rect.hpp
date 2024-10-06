e75917c1700c1b3ac44c7e75e824f862f5c52bde
Daniel Spiers <danjspiers@gmail.com>
2024-10-06 23:13:54 +0100

    C++ Classes Worksheet Started

---
+++ C++ Exercises/C++ Classes/rect.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+class Rectangle
+{
+    private:
+        double x, y;
+        double width, height;
+
+    public:
+        Rectangle(double x, double y, double width, double height)
+        {
+            
+        }
+};
