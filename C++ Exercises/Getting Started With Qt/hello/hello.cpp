d50d9cdd3d8403fff2fb58a917df81147620b868
Daniel Spiers <danjspiers@gmail.com>
2024-10-22 22:19:30 +0100

    Getting Started With Qt Done

--- C++ Exercises/Getting Started With Qt/hello/hello.cpp
+++ C++ Exercises/Getting Started With Qt/hello/hello.cpp
@@ -5,6 +5,15 @@ int main(int argc, char* argv[])
     QApplication app(argc, argv);
 
     QLabel* widget = new QLabel("<h1>Hello World!</h1>");
+
+    QFont font("Times", 14, QFont::Bold);
+    widget->setFont(font);
+    widget->setStyleSheet("color: red");
+    widget->setAlignment(Qt::AlignCenter);
+
+    widget->setMinimumSize(360, 120);
+
+
     widget->show();
 
     return app.exec();
